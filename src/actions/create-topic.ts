c9fa3f5fc3a4f7bf6f5b3d699a215ecadf6730ea
davoud nasiri <davoud.nsi@gmail.com>
2024-01-08 21:24:11 +0330

    create topic & revalidate with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,87 @@
 "use server";
 
-export async function createTopic() {
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  fromState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  // Check Sign In
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in"],
+      },
+    };
+  }
+
+  // Check Validation
+  if (!result.success) {
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  let topic: Topic;
+  try {
+    // throw new Error("failed");
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
+
+  // if all was OK
+  // return {
+  //   errors: {},
+  // };
+
   // TODO: revalidate the home page
 }
